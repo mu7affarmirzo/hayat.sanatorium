ba2c9c96d9ed935fd6e418f6b710de5b7db3653d
Kamronbek <shuxratmalikov08@gmail.com>
2023-12-20 16:44:17 +0500

    add Accont UI and SearchPatients UI in Patients
    Add 'Account' UI and 'SearchPatients' Ui in Patients

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,29 +1,30 @@
 export interface SvgProps {
-    width?: number;
-    height?: number;
-    viewBox?: string;
-    preserveAspectRatio?: string;
-    color?: string;
-    title?: string;
-    stroke?: string;
-    fill?: string;
-    strokeOpasity?: string;
+  width?: number;
+  height?: number;
+  viewBox?: string;
+  preserveAspectRatio?: string;
+  color?: string;
+  title?: string;
+  stroke?: string;
+  fill?: string;
+  strokeOpasity?: string;
+  style?: any;
 }
 export interface RoomsTypePriceView {
-    id: number;
-    roomNumber: string;
-    capacity: number;
-    level: number;
-    view: string;
-    isClean: string;
-    color: string;
-    departureDate: null;
-    visitDate: null;
-    gender: null;
-    age: null;
-    section: string;
-    cleaningType: string;
-    occupied: boolean;
-    comments: string;
-    howManyTimesUsed: number;
+  id: number;
+  roomNumber: string;
+  capacity: number;
+  level: number;
+  view: string;
+  isClean: string;
+  color: string;
+  departureDate: null;
+  visitDate: null;
+  gender: null;
+  age: null;
+  section: string;
+  cleaningType: string;
+  occupied: boolean;
+  comments: string;
+  howManyTimesUsed: number;
 }
