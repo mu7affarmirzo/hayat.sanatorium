6aab6b44641e80bc5660488302e82de1ced7311a
Kamronbek <shuxratmalikov08@gmail.com>
2023-12-28 13:01:11 +0500

    commit
    changed same UI

--- src/views/events/EventsView.tsx
+++ src/views/events/EventsView.tsx
@@ -1,7 +1,13 @@
+import { Grid } from '@mui/material';
+import CustomizedAccordions from 'components/CustomizedAccordions';
 import React from 'react';
 
 const EventsView = () => {
-    return <div>EventsView</div>;
+    return <Grid className='' container>
+        <CustomizedAccordions title='Параметры поиска:' childrenStyle={{ background: '#F5F5F5' }}>
+
+        </CustomizedAccordions>
+    </Grid>
 };
 
 export default EventsView;
